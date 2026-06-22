596cfb645808a5d0a3c8de7b2315849185b800b6
ANONYMOUS5545 <nzokaothniel@gmail.com>
2026-06-22 10:46:48 +0300

    NEXTJS ERROR

--- next.config.ts
+++ next.config.ts
@@ -20,9 +20,7 @@ const nextConfig: NextConfig = {
     },
   },
   compress: true,
-  swcMinify: true,
   productionBrowserSourceMaps: false,
-  optimizeFonts: true,
 };
 
 export default nextConfig;
